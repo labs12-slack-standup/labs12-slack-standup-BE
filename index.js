cff12408f75a2016e68bd812189d1bdf9e129829
AAsriyan <aasriyan@protonmail.com>
2019-04-24 19:29:29 -0400

    tinkering with configs

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 server.listen(process.env.PORT || 4000).then(({ url }) =>
-	console.log(`Server ready at ${url}`)
+	console.log(`🚀 Server ready at ${url}`)
 );
