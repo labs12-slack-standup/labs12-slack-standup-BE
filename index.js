d6ec519aa6318e9090003b7b3a62f57237c44d3b
AAsriyan <aasriyan@protonmail.com>
2019-04-24 19:17:05 -0400

    added dynamic port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen().then(({ url }) => console.log(`Server ready at ${url}`));
+server.listen(process.env.PORT || 4000).then(({ url }) =>
+	console.log(`Server ready at ${url}`)
+);
