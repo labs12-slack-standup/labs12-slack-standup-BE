96aa4b48d8ae06e4195a2e6147391ff0eaccea11
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-25 00:51:18 +0100

    add dynamic port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen().then(({ url }) => console.log(`Server ready at ${url}`));
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
