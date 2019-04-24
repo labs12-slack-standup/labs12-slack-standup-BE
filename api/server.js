c15448b6ec9d9ec28c1df423422291d007343820
AAsriyan <aasriyan@protonmail.com>
2019-04-24 19:37:43 -0400

    added config files

--- api/server.js
+++ api/server.js
@@ -2,7 +2,21 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers.js');
 
+const GRAPHQL_PLAYGROUND_CONFIG = {
+	folderName: 'Foo',
+	settings: {
+		'editor.cursorShape': 'line',
+		'editor.fontSize': 14,
+		'editor.reuseHeaders': true,
+		'editor.theme': 'dark'
+	}
+};
+
 const server = new ApolloServer({
+	playground:
+		process.env.NODE_ENV === 'production'
+			? false
+			: GRAPHQL_PLAYGROUND_CONFIG,
 	typeDefs,
 	resolvers
 });
