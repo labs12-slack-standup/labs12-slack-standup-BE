c30ed1174b3c742fe1e400767f1aa82cff3f1c35
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-25 01:09:02 +0100

    change production server variables

--- api/server.js
+++ api/server.js
@@ -2,9 +2,25 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers.js');
 
+const GRAPHQL_PLAYGROUND_CONFIG = {
+	folderName: 'Foo',
+	settings: {
+			'editor.cursorShape': 'line',
+			'editor.fontSize': 14,
+			'editor.reuseHeaders': true,
+			'editor.theme': 'dark'
+	}
+};
+
 const server = new ApolloServer({
+	playground:
+			process.env.NODE_ENV !== 'production'
+					? true
+					: GRAPHQL_PLAYGROUND_CONFIG,
+	introspection: true,
 	typeDefs,
 	resolvers
 });
+
 
 module.exports = server;
