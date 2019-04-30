465ddf305ed4fa316db47e17a3cefe3e62974878
erin-koen <erin.koen@gmail.com>
2019-04-30 13:14:17 -0400

    delete user by ID endpoint written and tested in postman, status 200 and 404 codes generated reliably

--- controllers/users.js
+++ controllers/users.js
@@ -20,10 +20,18 @@ router.get('/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
 		const user = await Users.findById(id);
-		res.status(200).json({
-			message: 'The user was retrieved successfully',
-			user
-		});
+
+		if (user) {
+			res.status(200).json({
+				message: 'The user was retrieved successfully',
+				user
+			});
+		} else {
+			res.status(404).json({
+				message:
+					'Sorry, the user requested does not exist'
+			});
+		}
 	} catch (error) {
 		res.status(500).json({
 			message:
@@ -46,13 +54,45 @@ router.put('/:id', async (req, res) => {
 				message: 'The user was edited succesfully.',
 				editedUser
 			});
+		} else {
+			res.status(400).json({
+				message:
+					'Please include a valid user property to edit.'
+			});
 		}
 	} catch (error) {
-		res.status(500).json({message: 'Sorry, there was an error when updating the user.'})
+		res.status(500).json({
+			message:
+				'Sorry, there was an error when updating the user.'
+		});
+		throw new Error(error);
 	}
 });
 
-//delete user by ID
-router.delete('/', (req, res) => {});
+//delete user by ID. Not actually sure we'll need this as we may just switch Active to false.
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await Users.findById(id);
+		if (user) {
+			await Users.remove(id);
+
+			res.status(200).json({
+				message:
+					'The user has been successfully removed.'
+			});
+		} else {
+			res.status(404).json({
+				message: 'Sorry, that user does not exist.'
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: 'Sorry, there was an error deleting the user.'
+		});
+		throw new Error(error);
+	}
+});
 
 module.exports = router;
