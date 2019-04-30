bad38a725b2eee067555d408e6c1666b03df1173
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-30 23:07:21 +0100

    add responses to report id

--- controllers/responses.js
+++ controllers/responses.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Responses = require('../models/Responses');
+const moment = require('moment');
 
 router.get('/', async (req, res) => {
 	try {
@@ -15,5 +16,28 @@ router.get('/', async (req, res) => {
 		throw new Error(error);
 	}
 });
+
+// This route will insert responses in the database with a reference to the report id
+router.post('/:reportId', async (req, res) => {
+	const userId = req.decodedJwt.subject										
+	const responseArr = req.body.responses.map(item => ({
+		reportId: req.params.reportId,
+		userId,														 		// @ Arshak, Erin, Mindy & Mikaela - question resource should not be dependent
+		question: item.question, 							// on the request body because the user could change it manually, we will need 
+		answer: item.answer,									// to pull the report from the db and compare the questions stringified questions
+		submitted_date: moment().format(),		// with the questions from the request body and see if they match, feel free to bring up in standup.
+	}));
+	try {
+		const result = await Responses.add(responseArr);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				'Sorry but something went wrong while posting your responses.'
+		});
+
+		throw new Error(error);
+	}
+})
 
 module.exports = router;
