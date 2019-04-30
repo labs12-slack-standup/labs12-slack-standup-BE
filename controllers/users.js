64945560c7fa6ed81d6ba7e0b83a7a1e870f93d3
erin-koen <erin.koen@gmail.com>
2019-04-30 12:47:55 -0400

    Edit user by userId written and tested for all conditionals in postman

--- controllers/users.js
+++ controllers/users.js
@@ -19,9 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
 	try {
 		const { id } = req.params;
-
 		const user = await Users.findById(id);
-
 		res.status(200).json({
 			message: 'The user was retrieved successfully',
 			user
@@ -35,5 +33,26 @@ router.get('/:id', async (req, res) => {
 		throw new Error(error);
 	}
 });
+
+//edit user by ID
+//what properties do we want to be editable?
+router.put('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { fullName, password, profilePic, active } = req.body;
+		if (fullName || password || profilePic || active) {
+			const editedUser = await Users.update(id, req.body);
+			res.status(200).json({
+				message: 'The user was edited succesfully.',
+				editedUser
+			});
+		}
+	} catch (error) {
+		res.status(500).json({message: 'Sorry, there was an error when updating the user.'})
+	}
+});
+
+//delete user by ID
+router.delete('/', (req, res) => {});
 
 module.exports = router;
