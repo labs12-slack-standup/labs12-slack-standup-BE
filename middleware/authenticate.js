cc31817bad1730688a79139be375968b0af99bae
shaunmcarmody <shaun.carmody@live.co.uk>
2019-05-07 01:10:36 +0100

    make post request to slack to approve code

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,30 +3,22 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
-	req.decodedJwt = {
-		subject: 1,
-		teamId: 1
+	const token = req.headers.authorization;
+
+	if (token) {
+		jwt.verify(token, secret, (error, decodedToken) => {
+			if (error) {
+				res.status(401).json({
+					message: 'You are not authorized.'
+				});
+			} else {
+				req.decodedJwt = decodedToken;
+				next();
+			}
+		});
+	} else {
+		res.status(401).json({
+			message: 'You have not provided a token.'
+		});
 	}
-	next();
-}
-
-// module.exports = (req, res, next) => {
-// 	const token = req.headers.authorization;
-
-// 	if (token) {
-// 		jwt.verify(token, secret, (error, decodedToken) => {
-// 			if (error) {
-// 				res.status(401).json({
-// 					message: 'You are not authorized.'
-// 				});
-// 			} else {
-// 				req.decodedJwt = decodedToken;
-// 				next();
-// 			}
-// 		});
-// 	} else {
-// 		res.status(401).json({
-// 			message: 'You have not provided a token.'
-// 		});
-// 	}
-// };
+};
