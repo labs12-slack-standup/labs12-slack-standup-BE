3cf735b2a2cb133699b5e25a873a4ea3a3f9848f
shaunmcarmody <shaun.carmody@live.co.uk>
2019-05-14 18:34:45 +0100

    change day method to subtract

--- controllers/responses.js
+++ controllers/responses.js
@@ -3,7 +3,7 @@ const Responses = require('../models/Responses');
 const Reports = require('../models/Reports');
 const moment = require('moment');
 const dateFns = require('date-fns');
-const { addDays } = require('date-fns');
+const { subDays } = require('date-fns');
 const searchReports = require('../helpers/searchReports');
 
 router.get('/', async (req, res) => {
@@ -35,6 +35,7 @@ router.post('/:reportId/day', async (req, res) => {
 		res.status(200).json([batch])
 	} catch (err) {
 		console.log(err);
+		throw new Error(err);
 	}
 });
 
@@ -52,7 +53,7 @@ router.get('/:reportId', async (req, res) => {
 
 		// Loop through the last 7 days and search reports for each day
 		for (let i = 0; i < 7; i ++) {
-			const day = addDays(date, i)
+			const day = subDays(date, i)
 			// const reports = await searchReports(reportId, day);
 			// console.log(reports);
 			const batch = {
@@ -61,92 +62,12 @@ router.get('/:reportId', async (req, res) => {
 			}
 			payload.push(batch);
 		}
-		console.log(payload);
 		res.status(200).json(payload);
 	} catch (err) {
 		console.log(err);
+		throw new Error(err);
 	}
 });
-
-// // Gets all reponses by report
-// router.get('/:reportId', async (req, res) => {
-// 	const { reportId } = req.params;
-// 	const { teamId } = req.decodedJwt;
-// 	const startday = dateFns.startOfDay(new Date());
-// 	const endDay = dateFns.endOfDay(new Date());
-// 	try {
-// 		// Run a check in the Reports model to verify that the reportId and TeamId are a match
-// 		// If teamId and reportId don't match with resource error will be thrown
-// 		await Reports.findById(reportId, teamId);
-// 		// reportId and teamId have passed verification check.
-// 		const responses = await Responses.findByAndJoin(reportId, startday, endDay);
-// 		// We need to parse each resource and insert into there own user object
-
-// 		// Create Members Array
-// 		let membersArray = [];
-
-// 		// Check if responses are in array
-// 		if (responses.length < 1) {
-// 			return res.status(204).json({
-// 				message: 'There are currently no reponses for this report.'
-// 			});
-// 		}
-
-// 		// Insert First Resource
-// 		membersArray.push({
-// 			userId: responses[0].userId,
-// 			fullName: responses[0].fullName,
-// 			questions: [
-// 				{
-// 					question: responses[0].question,
-// 					answer: responses[0].answer
-// 				}
-// 			]
-// 		});
-
-// 		// Start loop from second resource
-// 		for (let i = 1; i < responses.length; i++) {
-// 			const n = membersArray.length - 1;
-// 			// If the fullName of the current resource matches the fullName of the last resource in the
-// 			// membersArray push the questions to the questions property
-// 			if (membersArray[n].fullName === responses[i].fullName) {
-// 				membersArray[n].questions.push({
-// 					question: responses[i].question,
-// 					answer: responses[i].answer
-// 				});
-// 				// If the fullName's do not match insert a new object
-// 			} else {
-// 				membersArray.push({
-// 					userId: responses[i].userId,
-// 					fullName: responses[i].fullName,
-// 					questions: [
-// 						{
-// 							question: responses[i].question,
-// 							answer: responses[i].answer
-// 						}
-// 					]
-// 				});
-// 			}
-// 		}
-
-// 		if (responses.length === 0) {
-// 			res.status(404).json({ Message: 'no responses found' });
-// 		} else {
-// 			res.status(200).json({
-// 				message: 'Responses found in database',
-// 				membersArray
-// 			});
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(500).json({
-// 			message:
-// 				'Sorry but something went wrong while retrieving the list of responses by team.'
-// 		});
-
-// 		throw new Error(error);
-// 	}
-// });
 
 // This route will insert responses in the database with a reference to the report id
 router.post('/:reportId', async (req, res) => {
